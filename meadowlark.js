839a2690bca8cea9fa4e26b3ece9bbd6229b18ee
titov-andrei <titov_andrei@mail.ru>
2019-04-24 22:36:05 +0300

    ...

--- meadowlark.js
+++ meadowlark.js
@@ -2,34 +2,47 @@ var express = require('express');
 
 var app = express();
 
+// Установка механизма представления handlebars
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-app.get('/about', function (req, res) {
-  res.type('text/plain');
-  res.send('О Meadowlark Travel');
+app.get("/about", function(req, res) {
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomFortune });
 });
 
-// пользовательская страница 404
-app.use(function (req, res) {
-  res.type('text/plain');
+// Обобщенный обработчик 404 (промежуточное ПО)
+app.use(function(req, res, next) {
   res.status(404);
-  res.send('404 — Не найдено');
+  res.render("404");
 });
 
-// пользовательская страница 500
-app.use(function (err, req, res, next) {
+// Обработчик ошибки 500 (промежуточное ПО)
+app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 — Ошибка сервера');
+  res.render("500");
 });
 
 app.listen(app.get('port'), function () {
   console.log('Express запущен на http://localhost:' +
     app.get('port') + '; нажмите Ctrl+C для завершения.');
 });
+
+var fortunes = [
+  "Победи свои страхи, или они победят тебя.",
+  "Рекам нужны истоки.",
+  "Не бойся неведомого.",
+  "Тебя ждет приятный сюрприз.",
+  "Будь проще везде, где только можно."
+];
