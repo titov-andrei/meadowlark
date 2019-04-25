8cc746d3c97e11ad197dd71d8e96ef2da185dafe
titov-andrei <titov_andrei@mail.ru>
2019-04-25 10:03:39 +0300

    ...

--- meadowlark.js
+++ meadowlark.js
@@ -12,6 +12,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+// Здесь находятся маршруты...
+
 app.get("/", function(req, res) {
   res.render("home");
 });
